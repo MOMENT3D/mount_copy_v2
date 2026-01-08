984a8c922623db0873702d2422f96a0823ae318a
Infill_Pattern <sanghun.id@gmail.com>
2026-01-08 13:12:09 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "설치를 시작합니다..."
-sudo cp /home/biqu/mount_copy/mountcopy /usr/bin/
+sudo cp /home/biqu/mount_copy_v2/mountcopy /usr/bin/
 sudo chmod +x /usr/bin/mountcopy
-sudo cp /home/biqu/mount_copy/99-mountcopy.rules /etc/udev/rules.d/
-sudo cp /home/biqu/mount_copy/mountcopy@.service /etc/systemd/system/
+sudo cp /home/biqu/mount_copy_v2/99-mountcopy.rules /etc/udev/rules.d/
+sudo cp /home/biqu/mount_copy_v2/mountcopy@.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo udevadm control --reload-rules
 sudo udevadm trigger
